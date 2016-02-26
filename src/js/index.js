5e8f5648db03c8bf03cbfe73379be2f31cde51c0
twipixel <twipixel@naver.com>
2016-02-26 17:26:51 +0900

    dropzone 추가

--- src/js/index.js
+++ src/js/index.js
@@ -7,50 +7,16 @@ window.onload = startApplication.bind(this);
 
 
 function startApplication() {
-
     console.log('startApplication');
 
-    console.dir(Dropzone);
+    var dropzone = Dropzone.instances[0];
 
-    Dropzone.options.box = {
-        maxFilesize: 5,
-        addRemoveLinks: true,
-        dictResponseError: 'Server not Configured',
-        acceptedFiles: ".png,.jpg,.gif,.bmp,.jpeg",
-        init:function(){
-            console.log('aaaa');
-            var self = this;
-            // config
-            self.options.addRemoveLinks = true;
-            self.options.dictRemoveFile = "Delete";
-            //New file added
-            self.on("addedfile", function (file) {
-                console.log('new file added ', file);
-            });
-            // Send file starts
-            self.on("sending", function (file) {
-                console.log('upload started', file);
-                $('.meter').show();
-            });
+    dropzone.on('addedfile', function(file) {
+        console.log('addedfile', file);
+    });
 
-            // File upload Progress
-            self.on("totaluploadprogress", function (progress) {
-                console.log("progress ", progress);
-                $('.roller').width(progress + '%');
-            });
-
-            self.on("queuecomplete", function (progress) {
-                $('.meter').delay(999).slideUp(999);
-            });
-
-            // On removing file
-            self.on("removedfile", function (file) {
-                console.log(file);
-            });
-        },
-
-    };
-
-
-
+    dropzone.on('success', function(file, done){
+       console.log('aaaa', file, done);
+    });
 }
+
