2e2e844d957d9da62b5cfe363ed11d32417b3e39
twipixel <twipixel@naver.com>
2016-03-31 20:40:01 +0900

    레티나 디스플레이 적용 취소

--- src/js/nts/editor/ImageEditor.js
+++ src/js/nts/editor/ImageEditor.js
@@ -21,7 +21,7 @@ export class ImageEditor {
             backgroundColor: 0x000000
         });
 
-        /*var interactive = true;
+         /*var interactive = true;
          this.stage = new PIXI.Stage(0xE6E9EC, interactive);*/
         this.stage = new PIXI.Container(0xE6E9EC);
 
@@ -46,60 +46,20 @@ export class ImageEditor {
         var width = window.innerWidth;
         var height = window.innerHeight;
 
-        var canvas = this.canvas;
-        var context = this.context;
-        var devicePixelRatio, backingStoreRatio, ratio, auto;
-
-        // finally query the various pixel ratios
-        devicePixelRatio = window.devicePixelRatio || 1,
-            backingStoreRatio = context.webkitBackingStorePixelRatio ||
-                context.mozBackingStorePixelRatio ||
-                context.msBackingStorePixelRatio ||
-                context.oBackingStorePixelRatio ||
-                context.backingStorePixelRatio || 1,
-
-            ratio = devicePixelRatio / backingStoreRatio;
-
-        // ensure we have a value set for auto.
-        // If auto is set to false then we
-        // will simply not upscale the canvas
-        // and the default behaviour will be maintained
-        if (typeof auto === 'undefined') {
-            auto = true;
-        }
-
-        // upscale the canvas if the two ratios don't match
-        if (auto && devicePixelRatio !== backingStoreRatio) {
-
-            var oldWidth = canvas.width;
-            var oldHeight = canvas.height;
-
-            canvas.width = oldWidth * ratio;
-            canvas.height = oldHeight * ratio;
-
-            canvas.style.width = oldWidth + 'px';
-            canvas.style.height = oldHeight + 'px';
-
-            // now scale the context to counter
-            // the fact that we've manually scaled
-            // our canvas element
-            context.scale(ratio, ratio);
-        }
-
         /**
          * 캔버스 사이즈와 디스플레이 사이즈 설정
          * 레티나 그래픽 지원 코드
          */
-        //this.canvas.width = width * window.devicePixelRatio;
-        //this.canvas.height = height * window.devicePixelRatio;
-        //this.canvas.style.width = width + 'px';
-        //this.canvas.style.height = height + 'px';
+        this.canvas.width = width * window.devicePixelRatio;
+        this.canvas.height = height * window.devicePixelRatio;
+        this.canvas.style.width = width + 'px';
+        this.canvas.style.height = height + 'px';
 
         /**
          * PIXI renderer 리사이즈
          * PIXI 에게 viewport 사이즈 변경 알림
          */
-        this.renderer.resize(canvas.width, canvas.height);
+        this.renderer.resize(this.canvas.width, this.canvas.height);
         this.cropper.resize();
     }
 
