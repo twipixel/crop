12d82e74f7e7eef370e3f519ab4573453c35e48c
twipixel <twipixel@naver.com>
2017-06-09 10:59:53 +0900

    touch 다시 삭제

--- src/es6Crop/index.js
+++ src/es6Crop/index.js
@@ -1,17 +1,14 @@
 import {ImageEditor} from './nts/editor/ImageEditor';
 
 
-
 var editor;
 var image = document.getElementById('image');
 var texture = document.getElementById('texture');
 
+
 //window.onload = initailize.bind(this);
 window.onload = fastInitailize.bind(this);
 window.onresize = resizeWindow.bind(this);
-
-
-touchInit();
 
 
 function fastInitailize() {
@@ -44,39 +41,4 @@ function beginWithCanvas(texture, imageElement) {
 function resizeWindow() {
     if(editor)
         editor.resize();
-}
-
-
-function touchHandler(event) {
-    var touches = event.changedTouches,
-        first = touches[0],
-        type = "";
-    switch(event.type)
-    {
-        case "touchstart": type = "mousedown"; break;
-        case "touchmove":  type = "mousemove"; break;
-        case "touchend":   type = "mouseup";   break;
-        default:           return;
-    }
-
-    // initMouseEvent(type, canBubble, cancelable, view, clickCount,
-    //                screenX, screenY, clientX, clientY, ctrlKey,
-    //                altKey, shiftKey, metaKey, button, relatedTarget);
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent(type, true, true, window, 1,
-        first.screenX, first.screenY,
-        first.clientX, first.clientY, false,
-        false, false, false, 0/*left*/, null);
-
-    first.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-
-function touchInit() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
 }
